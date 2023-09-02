db3287fffe92d859b4a6c897939842abdb919b8e
Xsodex <flex513513@gmail.com>
2023-09-02 12:38:04 +0300

    Подключил materialyze

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// Скрипты materialize
+import "materialize-css/dist/js/materialize.js";
+
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
