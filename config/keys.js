081df063ca9f09643a27ec7bebd5343feb6b4ccc
xsodex <flex513513@gmail.com>
2025-03-30 10:24:53 +0300

    отпраивл данные

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,3 @@
-// module.exports = {
-//     mongoUri: 'mongodb+srv://xsodex:19731379User@avtoprokat-xs.ao62v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-//     jwt: 'xsodex',
-// };
-
 
 module.exports = {
     mongoUri: 'mongodb://127.0.0.1:27017/xsodex-crm',
